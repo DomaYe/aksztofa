9bf35590ccd65d5c7d0936cbbe669abbcd7822d9
Doma <bartadomonkos@gmail.com>
2022-01-20 08:49:15 +0100

    fh_gondol

--- akaszto.py
+++ akaszto.py
@@ -21,11 +21,14 @@ def cserel(feladvany, szo, betu):
     eredmeny += szo[-1]
     return eredmeny 
 
+def keres(feladvany, lista):
+    eredmeny=[]
+    for helyseg in lista:
+        if len(feladvany) == len(helyseg) and helyseg[0] == feladvany[0] and helyseg[-1] == feladvany[-1]:
+            eredmeny.append(helyseg)
+    return eredmeny
 
-
-
-def main():
-    helysegek = beolvas("helyek.txt")
+def gep_gondol(helysegek):
     feladat = random.choice(helysegek)
     kiirando = feladvany(feladat)
     print(kiirando)
@@ -48,7 +51,29 @@ def main():
         print("grat")
     else:
         print("jo bena vagy a gondolt szo: ", feladat)
+    
+        
 
+def fh_gondol(helysegek):
+    print("Gondolj egy magyar településre!")
+    kiirando=input("Írd be a feladványt:")
+    lehetsegesek = keres(kiirando, helysegek)
+    #print(lehetsegesek)
+    # while ciklus inkább, amíg több mint 1 lehetséges van!
+    if len(lehetsegesek) == 0:
+        print("Nincs ilyen település.")
+    elif len(lehetsegesek) == 1:
+        print(lehetsegesek[0], "a gondolt város.")
+    else:
+        print(lehetsegesek)
+
+
+
+
+def main():
+    helysegek = beolvas("helyek.txt")
+    gep_gondol(helysegek)
+    fh_gondol(helysegek)
     
             
       
